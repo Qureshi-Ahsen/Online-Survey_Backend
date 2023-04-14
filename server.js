1c9c92d18eb0d2fd92b65e78c6a8343aeb519830
Qureshi-Ahsen <ahsenqureshi5555@gmail.com>
2023-04-14 16:45:15 +0500

    made changes to code

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const port = 5000;
 const dotenv=require('dotenv').config();
 const routes=require('./router');
 const bodyParser=require('body-parser')
-const cors=require('cors')
+const cors = require('cors')
+app.use(cors());
  app.use(bodyParser.json());
  app.use(bodyParser.json({ type: 'application/*+json' }));
  app.use(bodyParser.urlencoded({ extended: false }));
 const connectDB = require('./config/database');
 app.use('/api',routes)
-app.use(cors());
+
 
 connectDB();
 app.use('*',(req,res)=>{
